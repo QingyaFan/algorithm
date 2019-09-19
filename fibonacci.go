702557210d0d1fe1e1d1feba70c81c7bec7152d6
cheerfun <qingyafan@163.com>
2019-09-20 00:56:36 +0800

    sync

--- fibonacci.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func Fibonacci() func() int {
-	x, y := 0, 1
-	return func() int {
-		x, y = y, x+y
-		return x
-	}
-}
--- sort/merge-sort.go
+++ sort/merge-sort.go
@@ -1,1 +1,23 @@
 package sort
+
+// mergeSort 归并排序
+func mergeSort(arr []int, left int, mid int, right int) {
+
+}
+
+func sort(arr []int, left int, right int) {
+	if left >= right {
+		return
+	}
+
+	mid := (left + right) / 2
+	sort(arr[0:mid], 0, mid)
+	sort(arr[(mid+1):], mid+1, right)
+
+	mergeSort(arr, left, mid, right)
+}
+
+// MergeSort merge sort
+func MergeSort(arr []int) []int {
+	sort()
+}
