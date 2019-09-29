38965494a72ed941b957369bffeb8fdb356d9a74
cheerfun <qingyafan@163.com>
2019-09-29 10:26:35 +0800

    feature: merge sort

--- sort/merge-sort.go
+++ sort/merge-sort.go
@@ -1,23 +1,42 @@
 package sort
 
 // mergeSort 归并排序
-func mergeSort(arr []int, left int, mid int, right int) {
+// O(nlogn) 或 O(nlog^2n)
+// O(n)+O(logn) 或 O(1)（自底向上）
+func mergeSort(array []int) []int {
+	n := len(array)
+	if n < 2 {
+		return array
+	}
+	key := n / 2
+	left := mergeSort(array[0:key])
+	right := mergeSort(array[key:])
 
+	return merge(left, right)
 }
 
-func sort(arr []int, left int, right int) {
-	if left >= right {
-		return
+func merge(left []int, right []int) []int {
+	newArr := make([]int, len(left)+len(right))
+	i, j, index := 0, 0, 0
+	for {
+		if left[i] > right[j] {
+			newArr[index] = right[j]
+			index++
+			j++
+			if j == len(right) {
+				copy(newArr[index:], left[i:])
+				break
+			}
+		} else {
+			newArr[index] = left[i]
+			index++
+			i++
+			if i == len(left) {
+				copy(newArr[index:], right[j:])
+				break
+			}
+		}
 	}
 
-	mid := (left + right) / 2
-	sort(arr[0:mid], 0, mid)
-	sort(arr[(mid+1):], mid+1, right)
-
-	mergeSort(arr, left, mid, right)
-}
-
-// MergeSort merge sort
-func MergeSort(arr []int) []int {
-	sort()
+	return newArr
 }
